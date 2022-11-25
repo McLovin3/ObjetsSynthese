f13ba9b39898b8ff246a17b2d025002ee709e2e1
Mathieu Ford <mathieu.ford@gmail.com>
2022-11-25 15:51:02 -0500

    Made forms work

--- app.py
+++ app.py
@@ -1,18 +1,34 @@
 from flask import Flask, render_template
-import packages.Freenove_DHT11 as DHT
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+# import packages.Freenove_DHT11 as DHT
 app = Flask(__name__)
-
-try:
-    dht = DHT.DHT(4)
-
-    verification = dht.readDHT11()
-    if (verification is dht.DHTLIB_OK):
-        temperature = dht.temperature
-        humidity = dht.humidity
-except:
-    temperature = humidity = "No sensor"
+app.config['SECRET_KEY'] = "McLovin"
 
 
-@app.route("/")
+class MessageForm(FlaskForm):
+    message = StringField("Message", [DataRequired()])
+    submit = SubmitField("Envoyer le message", render_kw={
+                         "class": "btn btn-secondary"})
+
+# try:
+#     dht = DHT.DHT(4)
+
+#     verification = dht.readDHT11()
+#     if (verification is dht.DHTLIB_OK):
+#         temperature = dht.temperature
+#         humidity = dht.humidity
+# except:
+#     temperature = humidity = "No sensor"
+
+
+@ app.route("/", methods=["GET", "POST"])
 def root():
-    return render_template("root.html", humidity=humidity, temperature=temperature)
+    form = MessageForm()
+    if form.validate_on_submit():
+        print(form.message.data)
+        form.message.data = ""
+
+    return render_template("root.html", form=form)
